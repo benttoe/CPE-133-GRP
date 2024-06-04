a1bfa59bacf796ec51142a1b646292c5394f2ceb
benttoe <61035368+benttoe@users.noreply.github.com>
2024-06-04 15:05:34 -0700

    Commented SEQ_STORAGE.sv

--- SEQ_STORAGE.sv
+++ SEQ_STORAGE.sv
@@ -19,22 +19,22 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
+//declaring in/out
 module SEQ_STORAGE(
     input BTN,
     input [7:0] LEDS,
     input CLK,
-    output logic [7:0] SEQ = 0
+    output logic [7:0] SEQ = 0 //initialize SEQ to be 0 by default
     );
     
     
 
     
-    always_ff @(posedge CLK)begin
+    always_ff @(posedge CLK) begin //button press only does something on the clock cycle to avoid timing issues
      
-    if(BTN)
-        SEQ <= LEDS;
-    else SEQ <= 8'b0;
+        if(BTN)
+            SEQ <= LEDS; //if the button is pressed the output becomes the position of the moving LED represented as an 8 bit sequence
+        else SEQ <= 8'b0; //otherwise the sequence becomes 8 0s: this guarantees that the FSM will always return 0 if the button is never pressed. 
         
     end   
     
